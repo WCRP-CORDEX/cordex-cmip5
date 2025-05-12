32ec02d5c06da2a8fe06e7a63558f631f4aaa48b
Jesus Fernandez <jesusff@users.noreply.github.com>
2025-05-12 11:28:02 +0200

    Fetch the list of missing data from latest version

--- esgf-data-loss/locate_available_data.sh
+++ esgf-data-loss/locate_available_data.sh
@@ -1,11 +1,16 @@
 #!/bin/bash
 
-basepath=$1
+basepath="$1"
+missing_data_url="https://raw.githubusercontent.com/WCRP-CORDEX/cordex-cmip5/refs/heads/main/esgf-data-loss/Missing_CORDEX_Data.txt"
 
-cat Missing_CORDEX_Data.txt | while read dataset
+if [ -z "$basepath" ]; then
+  echo "Usage: $0 /path/to/local/cordex/data"
+  exit 1
+fi
+
+curl -fsSL "$missing_data_url" | while read dataset
 do
   localpath=${basepath}/${dataset//./\/}
-#  echo ${localpath}
   if test -d "${localpath}"
   then
     echo "${dataset} -> found at ${localpath}"
